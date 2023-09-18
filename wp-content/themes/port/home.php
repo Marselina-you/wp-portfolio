7e7a087571ad19e330d6bb81d94aad37fe6ee39f
Marselina-you <marselina88@list.ru>
2023-09-18 09:18:26 +0300

    форма связи

--- wp-content/themes/port/home.php
+++ wp-content/themes/port/home.php
@@ -73,6 +73,27 @@ wp_reset_postdata();
 ?>
  </ul>
 </section>
+<section class="communication">
+  <div class="communication__container container">
+<div class="communication__left">
+      <form action="post" class="communication__form">
+        <h2 class="communication__title">Сотрудничество</h2>
+        <p class="communication__descr">Напишите мне</p>
+
+        <?php echo do_shortcode('[contact-form-7 id="806f147" title="Контактная форма"]')?>
+       
+      </form>
+          </div>
+          <div class="communication__right">
+    <a href="https://t.me/MarinaAlferova23" class="telegram-link">Телеграм: <span class="telegram-link__span">MarinaAlferova23</span></a>
+    <a href="https://wa.me/79201923888" class="telegram-link">Whatsup: <span class="telegram-link__span">+7(920)192-38-88</span></a>
+    <div class="communication__git">
+<span>Мой профиль на GitHub:</span>
+<a href="https://github.com/Marselina-you" class="communication__git-address">github.com/Marselina-you</a>
+    </div>
+    </div>
+</div>
+</section>
 </main>
 
 
